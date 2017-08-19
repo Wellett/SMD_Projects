8ac73e768d11140116aa09a3e1c1a4d029f4dc6a
William <wellett@student.unimelb.edu.au>
2017-08-20 02:20:11 +1000

    Complete basic implementation of My Mail Pool, and check for compilation errors

--- ProjectA/swen30006/strategies/MyMailPool.java
+++ ProjectA/swen30006/strategies/MyMailPool.java
@@ -5,59 +5,71 @@ Programming and Software Development SWEN30006
 MyMailPool
 
 Functions requiring implemetation
-- getPriorityPoolsize
-- getNonPriorityPoolSize
-- addToPool
-- getNonPriorityMail
 - getHighestPriorityMail
 - getBestMail
 */
+package strategies;
+
 import java.util.LinkedList;
 import automail.MailItem;
-
+import automail.PriorityMailItem;
 
 public class MyMailPool implements IMailPool{
   // MAIL POOL
   // Implement linked list as a queue for non priority mail items
-  private LinkedList<MailItem> nonPriorityPool;
-  // Implement linked list for Priority mail items
-  private LinkedList<MailItem> priorityPool;
+  private LinkedList<MailItem> nonPriorityQueue;
+  // Implement linked list for Priority mail
+  private LinkedList<MailItem> priorityQueue;
 
   // CONSTRUCTOR
   public MyMailPool(){
-    nonPriorityPool = new LinkedList<MailItem>();
-    priorityPool = new LinkedList<MailItem>();
+    nonPriorityQueue  = new LinkedList<MailItem>();
+    priorityQueue = new LinkedList<MailItem>();
   }
 
   //Returns the number of priority mail items in the pool
-  public int getPriorityPoolsize(){
-    return priorityPool.size();
+  public int getPriorityPoolSize(){
+    return priorityQueue.size();
   }
 
   //Returns the number of non-priority main items in the pool
   public int getNonPriorityPoolSize(){
-    return nonPriorityPool.size();
+    return nonPriorityQueue.size();
   }
 
   //Adds an item to the mail pool
   public void addToPool(MailItem mailItem){
-
+    if (mailItem instanceof PriorityMailItem){
+      // Add to priority pool
+      // currently FIFO, improve later
+      priorityQueue.addLast(mailItem);
+    }
+    else {
+      nonPriorityQueue.addLast(mailItem);
+    }
   }
 
-  // Returns the earliest arrived non-priority mail
+  // Returns the earliest arrived non-priority mail and deletes from queue
   // Mail items are taken from the end of the list and taken from the start
   public MailItem getNonPriorityMail(){
-    return nonPriorityPool.getFirst();
+    return nonPriorityQueue.removeFirst();
   }
 
   // Returns the earliest of the mail items with the highest priority in the
   // mail pool
+  //curretnly just returns the earliest (FIFO)
   public MailItem getHighestPriorityMail(){
-
+    if (getPriorityPoolSize() > 0){
+      return priorityQueue.removeFirst();
+    }
+    else {
+      return null;
+    }
   }
 
   // Returns the best MailItem to deliver
+  // currently not implemented
   public MailItem getBestMail(int FloorFrom, int FloorTo){
-
+    return null;
   }
 }
