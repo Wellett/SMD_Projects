d2652deab2436dc5cd2fa8fd176e1552405b0beb
William <wellett@student.unimelb.edu.au>
2017-08-19 23:01:54 +1000

    Add blank implementation classes:

---
+++ ProjectA/swen30006/strategies/MyMailPool.java
@@ -0,0 +1,10 @@
+/* Author: William Ellett
+StudentID: 586703
+Last Modified: 19/8/2017
+Programming and Software Development SWEN30006
+MyMailPool
+*/
+
+public class MyMailPool implements IMailPool{
+
+}
---
+++ ProjectA/swen30006/strategies/MyRobotBehaviour.java
@@ -0,0 +1,10 @@
+/* Author: William Ellett
+StudentID: 586703
+Last Modified: 19/8/2017
+Programming and Software Development SWEN30006
+MyRobotBehaviour
+*/
+
+public class MyRobotBehaviour implements IRobotBehaviour{
+
+}
